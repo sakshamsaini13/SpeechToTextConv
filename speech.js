f59f3415f99175101e658329f2859888deaf8563
sakshamsaini13 <77584746+sakshamsaini13@users.noreply.github.com>
2023-07-13 00:49:12 +0530

    third commit

--- speech.js
+++ speech.js
@@ -54,8 +54,12 @@ var messages = {
   let recognition;
 
 
-  let selectLanguage=document.querySelector("#select-language");
-  let selectDialect=document.querySelector("#select-dialect");
+  let selectLanguage=document.querySelector("#select_language");
+  let selectDialect=document.querySelector("#select_dialect");
+
+let final_span=document.getElementById("final_span");
+let interim_span=document.getElementById("interim_span");
+
 
   document.addEventListener("DOMContentLoaded",function()
   {
@@ -63,4 +67,139 @@ var messages = {
     {
         selectLanguage.options[i]=new Option(langs[i][0],i);
     }
-  });
+    selectLanguage.selectedIndex=10;
+
+    updateCountry();
+    if(!("webkitSpeechRecognition" in window))
+    {
+        showinfo("upgrade");
+    }
+    else
+    {
+        showinfo("start");
+        recognition=new webkitSpeechRecognition();
+        recognition.continuous=true; // continusly record karta rahega
+        recognition.onstart=function() // agar mei dot .om start likha matlab revogniton start ho jayega aur jo maine fuxn banaya hai vo chalega
+        {
+            recognizing=true;
+            showinfo("speak_now");
+            start_img.src="images/mic-animation.gif";
+        }
+        recognition.onerror=function(event)
+        {
+            if(event.error=="not-allowed")
+            {
+                showinfo("blocked");
+            }
+            else 
+            {
+                showinfo("problem");
+            }
+            onend_error=true;
+        }
+
+        recognition.onend=function()
+        {
+            recognizing=false;
+            if(onend_error)return;
+            start_img.src="images/mic.gif";
+            if(!finalTranscript)
+            {
+                showinfo("start");
+                return;
+            }
+            showinfo("stop");
+        }
+        recognition.onresult=function(event)
+        {  
+            console.log(event);
+            let interim_transcript="";
+            for(i=event.resultIndex;i<event.results.length;i++)
+            {
+                if(event.results[i].isFinal)
+                {
+                    finalTranscript+=event.results[i][0].transcript;   // ye multuiple baar chalega aur jis results[i] pe isFINAL HOGa uspe result store hoga baaki pe nhi ho paayega
+                } 
+                else
+                {
+                    interim_transcript+=event.results[i][0].transcript;
+                }
+            }
+            final_span.innerHTML=finalTranscript;
+            interim_span.innerHTML=interim_transcript;
+        }
+      
+    }
+ });
+
+ 
+ function updateCountry()
+ {
+    for(let i=selectDialect.length-1;i>=0;i--)
+    {
+        selectDialect.remove(i);
+    }
+    let list =langs[selectLanguage.selectedIndex];
+    for(let i=1;i<list.length;i++)
+    {
+        selectDialect.options.add(new Option(list[i][1],list[i][0]));
+    }
+    selectDialect.style.visibility=list[1].length==1?"hidden":"visible";
+ }
+
+ document.getElementById("select_language").addEventListener("change",function()
+ {
+   updateCountry();
+ });
+
+ document.getElementById("start_button").addEventListener("click",function()
+ {
+     if(recognizing)
+     {
+        recognition.stop();
+        return;
+     }
+     console.log("hello");
+     finalTranscript='';
+     recognition.lang=selectDialect.value;
+     recognition.start();
+     final_span.innerHTML="";
+     interim_span.innerHTML="";
+ });
+
+ document.getElementById("copy_button").addEventListener("click",function()
+ {
+    if(recognizing)
+    {
+        recognizing=false;
+        recognition.stop();
+    }
+    copyToClipboard(final_span.innerText);
+
+ });
+ function copyToClipboard(text)
+ {  
+    // create a element
+    const el=document.createElement("textarea");
+    el.value=text;
+    document.body.appendChild(el);
+    el.select();
+    document.execCommand("copy");
+    document.body.removeChild(el);
+    showinfo("copy");
+ }
+
+ function showinfo(s)
+ { 
+    let info=document.getElementById("info");
+    if(s)
+    {
+        let message=messages[s];
+        info.innerHTML=message.msg;
+        info.className="alert "+message.class;
+    }
+    else
+    {
+        info.className="d-none";
+    }
+ }
